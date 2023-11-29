436a6155af05de336ba8b884e8a17f7151296340
Sri <srikameswari.talluri@gmail.com>
2023-11-29 22:29:12 +0530

    Step 3

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -6,6 +6,10 @@ function nextSeq()
     var randNum = Math.floor(Math.random()*3); 
     var randCol = buttonCol[randNum];
     gamePattern.push(randCol);
+
+    $("#"+randCol).fadeOut(100).fadeIn(100);
+    $("sounds/"+randCol+".mp3").play();
+    //var audio = new Audio("sounds/" + randomChosenColour + ".mp3"); audio.play();
 }
 
 
