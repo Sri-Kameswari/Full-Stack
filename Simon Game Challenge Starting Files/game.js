e48743fc95b12ee16b2860fa532a4eccc358e3db
Sri <srikameswari.talluri@gmail.com>
2023-11-29 21:51:01 +0530

    Step 2

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,12 @@
-alert("Hello!");
+var buttonCol = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSeq()
+{
+    var randNum = Math.floor(Math.random()*3); 
+    var randCol = buttonCol[randNum];
+    gamePattern.push(randCol);
+}
+
+
+
