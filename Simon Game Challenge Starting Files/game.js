892fd94f56374ad5e2f86f58956dd0fc803d49d3
Sri <srikameswari.talluri@gmail.com>
2023-11-29 22:40:51 +0530

    Step 5

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -10,15 +10,19 @@ $(".btn").click(function(){
 
 function nextSeq()
 {
-    var randNum = Math.floor(Math.random()*3); 
+    var randNum = Math.floor(Math.random()*4); 
     var randCol = buttonCol[randNum];
     gamePattern.push(randCol);
-
     $("#"+randCol).fadeOut(100).fadeIn(100);
+    playSound(randCol);
+}
+
+function playSound(randCol)
+{
+    
     $("sounds/"+randCol+".mp3").play();
     //var audio = new Audio("sounds/" + randomChosenColour + ".mp3"); 
     //audio.play();
-
 }
 
 
