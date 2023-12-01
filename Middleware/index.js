073a68625c182cdac0b7137e8eece9d6940838b0
Sri <srikameswari.talluri@gmail.com>
2023-12-01 13:12:21 +0530

    Morgan middleware

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,10 +1,14 @@
 import express from "express";
-import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan("combined"));
+
+app.get("/", (req, res)=>{
+    res.send("Hello");
+});
 
 app.post("/submit", (req, res)=>{
     console.log(req.body);
